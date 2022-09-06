bad97f07e3454c62d303e940c4059e239834cac7
Vitor <vitor.franca.9400@gmail.com>
2022-09-06 09:14:54 -0300

    feat: Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
+import Header from "./components/Header"
 function App() {
   return (
     <>
-      <h1>Olá Mundo!</h1>
-      <NotificationButton/>
+      <Header/>
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 }

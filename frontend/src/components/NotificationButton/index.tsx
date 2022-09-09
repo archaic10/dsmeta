72d3d7c51630a807bcac079636bac1c1528af848
Vitor <vitor.franca.9400@gmail.com>
2022-09-09 19:45:18 -0300

    feat: add message with toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,7 +1,8 @@
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
+import { toast } from 'react-toastify';
 import axios from "axios";
 import './styles.css';
-import { BASE_URL } from '../../utils/request';
 
 type Props = {
   saleId: number
@@ -10,7 +11,13 @@ type Props = {
 function handleClick(saleId:number){
   axios.get(`${BASE_URL}/sales/${saleId}/notification`)
     .then(()=>{
-      console.log("Mensagem enviada com sucesso!")
+      toast.success("SMS enviada com sucesso!", {
+        position: toast.POSITION.TOP_CENTER
+      })
+    }).catch(()=>{
+      toast.error("Erro ao enviar SMS!", {
+        position: toast.POSITION.TOP_CENTER
+      })
     });
 }
 
